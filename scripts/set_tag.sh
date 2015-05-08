c3345347780c721453e88147c8d7beec9790b5b9
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-05-08 17:46:57 -0300

    correção no script set_tag

--- scripts/set_tag.sh
+++ scripts/set_tag.sh
@@ -1,28 +1,35 @@
+function fetch_settag {
+
+	git fetch --all
+	git checkout $1
+
+}
+
 cd ..
 
-git checkout $1
+fetch_settag $1
 
 cd src/wp-content/themes/participacao-tema
 
-git checkout $1
+fetch_settag $1
 
 cd ../pensandoodireito-tema
 
-git checkout $1
+fetch_settag $1
 
 cd ../marcocivil-tema
 
-git checkout $1
+fetch_settag $1
 
 cd ../dadospessoais-tema
 
-git checkout $1
+fetch_settag $1
 
 cd ../../plugins/pensandoodireito-network-functions
 
-git checkout $1
+fetch_settag $1
 
 cd ../wp-side-comments
 
-git checkout $1 
+fetch_settag $1 
 
