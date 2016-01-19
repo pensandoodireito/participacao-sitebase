785408bff66219e62ed26c703ccca89d60c767f0
Josafá Filho <josafafilho15@gmail.com>
2016-01-18 22:22:48 -0200

    Ajusta link para pasta com a documentação da API Atualiza informações da API

--- post-update.sh
+++ post-update.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 cp config/wp-config-vagrant.php src/wp-config.php
 cp config/htaccess-vagrant src/.htaccess
 cp config/file-remote.php src/file-remote.php
@@ -5,4 +7,4 @@ cp config/index-vagrant.php src/index.php
 rm -f src/wp/index.php
 ln -s wp/wp-admin src/wp-admin
 ln -s wp/wp-includes src/wp-includes
-ln -s docs src/docs
+ln -s ../docs docs
